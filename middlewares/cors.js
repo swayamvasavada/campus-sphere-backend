0c227dda3bde61663f7969d99b4bd4390ccc1d43
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2024-01-17 13:16:59 +0530

    Added CORS support

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,7 @@
 function enableCors(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-Access-Token');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
 }
