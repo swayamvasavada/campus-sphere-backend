3196555ea888aa5ccfa9ed26ccdd2b5c81909dbc
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2025-06-30 19:48:38 +0530

    Updated Batch and Dept module

--- routes/batch.routes.js
+++ routes/batch.routes.js
@@ -1,45 +1,169 @@
-const db = require('../data/database');
 const router = require('express').Router();
+const ObjectId = require('mongodb').ObjectId
+
+const db = require('../data/database');
 
 router.get('/', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
     const result = await db.getDb().collection('batch').find().toArray();
 
-    res.json(result)
-})
+    res.json({
+        hasError: false,
+        data: result
+    });
+});
+
+router.get('/summary', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+    let result;
+
+    try {
+        result = await db.getDb().collection('batch').countDocuments();
+    } catch (error) {
+        console.log("Error: ", error);
+        next(error);
+    }
+
+    return res.json({
+        hasError: false,
+        result: result
+    });
+});
+
+router.get('/:batchId', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+
+    const batchId = req.params.batchId;
+    try {
+        const result = await db.getDb().collection('batch').findOne({ _id: new ObjectId(batchId) });
+        if (result._id) {
+            return res.json({
+                hasError: false,
+                data: result
+            });
+        }
+
+        return res.status(404).json({
+            hasError: true,
+            message: "Batch not found!"
+        });
+    } catch (error) {
+        console.log("Error: ", error);
+        next(error);
+    }
+});
 
 router.post('/create-batch', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+
     const batchData = req.body;
     let result;
 
     try {
-        const deptList = await db.getDb().collection('dept').find({}, { projection: { deptName: 1 } }).toArray();
-
         const data = {
             year: batchData.year,
             noOfSemester: batchData.noOfSemester,
-            fees: {}
+            fees: []
         }
 
         for (let i = 1; i <= batchData.noOfSemester; i++) {
-            data.fees[`semester${i}Fees`] = batchData[`semester${i}Fees`];
-            data.fees[`fees${i}Due`] = batchData[`fees${i}Due`];
-        }
-        
-        let i = 0;
+            const fees = {};
+            fees.semesterNo = +i;
+            fees.fees = batchData[`semester${i}Fees`];
+            fees.dueDate = batchData[`fees${i}Due`];
 
-        while (i != deptList.length) {
-            const branch = deptList[i].deptName
-            data[branch] = { mentorId: null, mentorName: null };
-            i++;
+            data.fees.push(fees);
         }
 
         result = await db.getDb().collection('batch').insertOne(data);
     } catch (error) {
         console.log(error);
-        next();
+        next(error);
     }
 
-    res.json(result);
+    res.json({
+        hasError: false,
+        message: "Batch created successfully"
+    });
+});
+
+router.post('/update-batch/:batchId', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+
+    const batchData = req.body;
+    const batchId = req.params.batchId;
+    let result;
+
+    try {
+        const data = {
+            year: batchData.year,
+            noOfSemester: batchData.noOfSemester,
+            fees: []
+        }
+
+        for (let i = 1; i <= batchData.noOfSemester; i++) {
+            const fees = {};
+            fees.semesterNo = +i;
+            fees.fees = batchData[`semester${i}Fees`];
+            fees.dueDate = batchData[`fees${i}Due`];
+
+            data.fees.push(fees);
+        }
+
+        result = await db.getDb().collection('batch').updateOne({ _id: new ObjectId(batchId) }, { $set: data });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+
+    res.json({
+        hasError: false,
+        message: "Batch created successfully"
+    });
+});
+
+router.post('/delete-batch/:batchId', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+
+    const batchId = req.params.batchId;
+
+    let result;
+    try {
+        result = await db.getDb().collection('batch').deleteOne({ _id: new ObjectId(batchId) });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+
+    if (result.deletedCount) {
+        return res.json({
+            hasError: false,
+            messaage: "Batch deleted successfully!"
+        });
+    }
+
+    res.json({
+        hasError: true,
+        message: "Something went wrong!"
+    });
 });
 
 module.exports = router;
--- routes/dept.routes.js
+++ routes/dept.routes.js
@@ -4,59 +4,108 @@ const ObjectId = require('mongodb').ObjectId;
 const db = require('../data/database');
 
 router.get('/', async function (req, res, next) {
+    if (res.locals.desg !== "Admin" && res.locals.desg !== "Librarian") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized roles can access this data"
+    });
+
     const result = await db.getDb().collection('dept').find().toArray();
 
-    res.json(result);
-})
+    res.json({
+        hasError: false,
+        data: result
+    });
+});
+
+router.get('/summary', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+    let result;
+
+    try {
+        result = await db.getDb().collection('dept').countDocuments();
+    } catch (error) {
+        console.log("Error: ", error);
+        next(error);
+    }
+
+    return res.json({
+        hasError: false,
+        result: result
+    });
+});
 
 router.get('/:deptId', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+
     const deptId = req.params.deptId;
     const result = await db.getDb().collection('dept').findOne({ _id: new ObjectId(deptId) });
 
-    res.json(result);
+    res.json({
+        hasError: false,
+        data: result
+    });
 });
 
 router.post('/create-dept', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+
     const deptData = req.body;
     let result;
 
     try {
         result = await db.getDb().collection('dept').insertOne({
             deptName: deptData.deptName,
-            hodInfo: null
         })
     } catch (error) {
         console.log(error);
-        next();
+        next(error);
     }
 
     res.json(result);
 });
 
 router.post('/update-dept/:deptId', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
 
-    const deptId = req.params.deptId;
     const deptData = req.body;
-
-    let userData = await db.getDb().collection('users').findOne({ _id: new ObjectId(deptData.hodId) }, { projection: { name: 1 } });
+    const deptId = req.params.deptId;
     let result;
-    const deptInfo = {
-        deptName: deptData.deptName,
-        hodInfo: {
-            hodId: new ObjectId(userData._id),
-            hodName: userData.name.firstName + " " + userData.name.lastName
-        }
-    }
+
     try {
-        result = await db.getDb().collection('dept').updateOne({ _id: new ObjectId(deptId) }, { $set: deptInfo });
+        result = await db.getDb().collection('dept').updateOne({ _id: new ObjectId(deptId) }, {
+            $set: {
+                deptName: deptData.deptName,
+            }
+        });
     } catch (error) {
         console.log(error);
-        next();
+        next(error);
     }
-    res.json(result);
+
+    return res.json({
+        hasError: false,
+        message: 'Department updated successfully'
+    });
 });
 
 router.post('/delete-dept/:deptId', async function (req, res, next) {
+    if (res.locals.desg !== "Admin") return res.status(403).json({
+        hasError: true,
+        message: "Only Authorized Admin can access this data"
+    });
+
     const deptId = req.params.deptId;
 
     let result;
@@ -64,10 +113,13 @@ router.post('/delete-dept/:deptId', async function (req, res, next) {
         result = await db.getDb().collection('dept').deleteOne({ _id: new ObjectId(deptId) });
     } catch (error) {
         console.log(error);
-        next();
+        next(error);
     }
 
-    res.json(result);
-})
+    return res.json({
+        hasError: false,
+        messaage: "Department deleted successfully!"
+    });
+});
 
 module.exports = router;
