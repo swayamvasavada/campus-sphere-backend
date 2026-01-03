862f84b8a48757b9a998871c1543bfa48a8a4948
Swayam Vasavada <swayam.vasavada.ordex@gmail.com>
2026-01-03 23:05:39 +0530

    Enabled scalability

--- app.js
+++ app.js
@@ -1,51 +1,13 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const cluster = require('cluster');
+const os = require('os');
+const server = require('./server');
 
-const dotenv = require('dotenv').config();
+const numCpus = os.cpus().length;
 
-const db = require('./data/database');
-const authRoutes = require('./routes/auth.routes');
-const enquiryRoutes = require('./routes/enquiry.routes');
-const userRoutes = require('./routes/user.routes');
-const batchRoutes = require('./routes/batch.routes');
-const deptRoutes = require('./routes/dept.routes');
-const libraryRoutes = require('./routes/library.routes');
-const noticeRoutes = require('./routes/notices.routes');
-const feesRoutes = require('./routes/fees.routes');
-const paymentHandleRoutes = require('./routes/payment-handle.routes');
-const tokenVerification = require('./middlewares/jwt-verify');
-
-const app = express();
-
-app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-}));
-
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(authRoutes);
-app.use(paymentHandleRoutes);
-app.use(tokenVerification);
-app.use('/enquiry', enquiryRoutes);
-app.use(userRoutes);
-app.use('/dept', deptRoutes);
-app.use('/batch', batchRoutes);
-app.use('/library', libraryRoutes);
-app.use('/notice', noticeRoutes);
-app.use('/fees', feesRoutes);
-
-app.use(function (error, req, res, next) {
-    console.log(error);
-    res.status(500).json({
-        hasError: true,
-        message: 'Something went wrong!',
-    });
-});
-
-db.connectToDatabase().then(function () {
-    console.log(`Server started on PORT ${process.env.PORT}`);
-    app.listen(process.env.PORT);
-});
+if (cluster.isPrimary) {
+    for (let i = 0; i < numCpus; i++) {
+        cluster.fork();        
+    }
+} else {
+    server();
+}
